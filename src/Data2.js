c9232b4eb7d1192c85e86ac14f7bea485e055b88
NazuriM <76538892+NazuriM@users.noreply.github.com>
2021-02-20 23:18:45 -0500

    Final

---
+++ src/Data2.js
@@ -0,0 +1,117 @@
+const data = {
+  status: "success",
+  items: [
+    {
+      id: 1,
+      title: "Sweater para dama",
+      price: 12,
+      image: "https://img.ltwebstatic.com/images3_pi/2020/11/20/160584720104f8d8f9b1818659d19546bb2ba93985_thumbnail_600x.webp",
+      category: "Nanai",
+      img1: "https://img.ltwebstatic.com/images3_pi/2020/11/20/160584720104f8d8f9b1818659d19546bb2ba93985_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      title: "Top tejida",
+      price: 15,
+      image: "https://img.ltwebstatic.com/images3_pi/2021/01/11/161034112155906c02d0b6e05c28bf6a56e5c86758_thumbnail_600x.webp",
+      category: "Nanai",
+      img1: "https://img.ltwebstatic.com/images3_pi/2021/01/11/161034112155906c02d0b6e05c28bf6a56e5c86758_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606319825&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 3,
+      title: "Falda con botón delantero",
+      price: 4,
+      image: "https://img.ltwebstatic.com/images3_pi/2020/09/21/1600653408eebed2161d35c651b4192d5c782ba04d_thumbnail_600x.webp",
+      category: "Nanai",
+      img1: "https://img.ltwebstatic.com/images3_pi/2020/09/21/1600653408eebed2161d35c651b4192d5c782ba04d_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325289&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 4,
+      title: "Conjunto con estampado floral",
+      price: 39,
+      image: "https://img.ltwebstatic.com/images3_pi/2020/04/29/15881487472fcfcec7770ea53ff9b55c1f467b35e2_thumbnail_600x.webp",
+      category: "Nanai",
+      img1: "https://img.ltwebstatic.com/images3_pi/2020/04/29/15881487472fcfcec7770ea53ff9b55c1f467b35e2_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325338&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 5,
+      title: "Camiseta con estampado de slogan",
+      price: 12,
+      image: "https://img.ltwebstatic.com/images3_pi/2020/12/15/160799771565a0d79a05000e63cc1ff10208b0b19e_thumbnail_600x.webp",
+      category: "Nanai",
+      img1: " https://img.ltwebstatic.com/images3_pi/2020/12/15/160799771565a0d79a05000e63cc1ff10208b0b19e_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606322330&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 6,
+      title: "Camisa de rayas de cuello revere",
+      price: 15,
+      image: "https://img.ltwebstatic.com/images3_pi/2020/07/01/1593587064b81e4b1d3c556fc97615bbbd8ad0586a_thumbnail_600x.webp",
+      category: "Nanai",
+      img1: "https://img.ltwebstatic.com/images3_pi/2020/07/01/1593587064b81e4b1d3c556fc97615bbbd8ad0586a_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606321824&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 7,
+      title: "Camiseta con estampado de slogan",
+      price: 10000,
+      image: "https://img.ltwebstatic.com/images3_pi/2020/12/03/160696195416b520c9c0849a011a9ab1047a7ef7f7_thumbnail_600x.webp",
+      category: "Nanai",
+      img1: "https://img.ltwebstatic.com/images3_pi/2020/12/03/160696195416b520c9c0849a011a9ab1047a7ef7f7_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606320966&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 8,
+      title: "Pantalones cargo con bolsillo con solapa",
+      price: 35,
+      image: "https://img.ltwebstatic.com/images3_pi/2020/05/27/15905689114b2821be750e3267c6a3db4c2ff28bc7_thumbnail_600x.webp",
+      category: "Nanai",
+      img1: "https://img.ltwebstatic.com/images3_pi/2020/05/27/15905689114b2821be750e3267c6a3db4c2ff28bc7_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325265&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 9,
+      title: "Pantalones plomo",
+      price: 12,
+      image: "https://img.ltwebstatic.com/images3_pi/2020/11/28/1606553818a9011da8f46405ef8b80750f537ea83f_thumbnail_600x.webp",
+      category: "Nanai",
+      img1: "https://img.ltwebstatic.com/images3_pi/2020/11/28/1606553818a9011da8f46405ef8b80750f537ea83f_thumbnail_600x.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606323250&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 10,
+      title: "Bolso cartera con cadena",
+      price: 51,
+      image: "https://img.ltwebstatic.com/images3_pi/2021/01/15/1610689763e6e89ae4b5df5fcdfcfce27805de5bc9.webp",
+      category: "Nanai",
+      img1: "https://img.ltwebstatic.com/images3_pi/2021/01/15/1610689763e6e89ae4b5df5fcdfcfce27805de5bc9.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1609445259&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 11,
+      title: "Carteras al por mayor amiguita",
+      price: 7,
+      image: "https://www.mangoandsalt.com/wp-content/uploads/2018/09/friperies-amsterdam-avis.jpg",
+      category: "Nanai",
+      img1: "https://www.mangoandsalt.com/wp-content/uploads/2018/09/friperies-amsterdam-avis.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1609438911&w=1000",
+      cantidad: 1
+    },
+  ],
+};
+
+export default data;
